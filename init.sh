9f30dcd2e88718353f91d7a14e96be8d9c4f1479
Amund Tenstad <github@amund.io>
2022-03-25 21:46:02 +0100

    Add albert and gnome settings

--- init.sh
+++ init.sh
@@ -1,9 +1,14 @@
-mkdir -p ~/coding ~/.bashrc.d
+mkdir -p ~/coding ~/programs ~/.bashrc.d 
 [ -d ~/coding/dotfiles ] || git clone git@github.com:tenstad/dotfiles.git ~/coding/dotfiles
-
-[ -d ~/.fzf ] || git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-yes | ~/.fzf/install
-mv ~/.fzf.bash ~/.bashrc.d/fzf
-sed -i '/\.fzf\.bash/d' ~/.bashrc
-
 sh ~/coding/dotfiles/sync.sh
+
+[ -d ~/programs/fzf ] || git clone --depth 1 https://github.com/junegunn/fzf.git ~/programs/fzf
+~/programs/fzf/install --key-bindings --completion --no-update-rc
+mv ~/.fzf.bash ~/.bashrc.d/fzf
+
+sudo rpm --import https://build.opensuse.org/projects/home:manuelschneid3r/public_key
+sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_34/home:manuelschneid3r.repo
+sudo dnf install -y albert
+
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+gsettings set org.gnome.desktop.interface cursor-size 36
