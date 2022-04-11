5fb7f2a7530203c18782d5d290e6d3144e39ea97
Amund Tenstad <github@amund.io>
2022-04-11 08:44:44 +0200

    init: add dnf update

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-mkdir -p ~/coding ~/programs ~/.bashrc.d 
+mkdir -p ~/coding ~/programs ~/.bashrc.d
+
+sudo dnf upgrade -y
 
 [ -d ~/programs/fzf ] || git clone --depth 1 https://github.com/junegunn/fzf.git ~/programs/fzf
 ~/programs/fzf/install --key-bindings --completion --no-update-rc
@@ -8,6 +10,7 @@ sudo rpm --import https://build.opensuse.org/projects/home:manuelschneid3r/publi
 sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/home:manuelschneid3r/Fedora_34/home:manuelschneid3r.repo
 sudo dnf install -y albert
 
+#gnome-extension 2398 600 4839 1500
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
 gsettings set org.gnome.desktop.interface cursor-size 36
 
